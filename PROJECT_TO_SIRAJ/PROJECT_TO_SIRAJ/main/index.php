825516989127da90b8f93b964b75126ac8c74126
MarkDeVv2 <148472565+MarkDeVv2@users.noreply.github.com>
2023-11-12 14:07:08 +0800

    Add files via upload

---
+++ PROJECT_TO_SIRAJ/PROJECT_TO_SIRAJ/main/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OverView</title>
+    <link rel="stylesheet" href="/PROJECT_TO_SIRAJ/main/Homestyle.php">
+   
+
+    
+    
+</head>
+<body>
+    <div class="nav">
+        <img src="/PROJECT_TO_SIRAJ/main/img/mainlogo.png" alt="logo" class="logo">
+        <img src="/PROJECT_TO_SIRAJ/main/img/logoname.png" alt="" class="logon">
+        <a href="#" >Home</a>
+        <a href="Books.php" class="bookss">Books</a>
+        <a href="#">About</a>
+        <a href="#">Feedback</a>
+        <a href="/PROJECT_TO_SIRAJ/student/login_student.php">Log in</a>
+    </div>
+    <div class="memberlog">
+        <img src="/PROJECT_TO_SIRAJ/main/img/lock.png" class="lockp" >
+    </div>
+
+
+    <h2 class="newarrh2">New Arrival</h2>
+    <div class="slide hi-slide">
+        
+        <div class="hi-prev "></div>
+          <div class="hi-next "></div>
+          <ul>
+              <li><img src="/PROJECT_TO_SIRAJ//main/booksimg/b1.png" alt="Img 1" id="Img1"/></li>
+              <li><img src="/PROJECT_TO_SIRAJ//main/booksimg/b4.png" alt="Img 2" id="Img2"/></li>
+              <li><img src="/PROJECT_TO_SIRAJ//main/booksimg/b2.png" alt="Img 3" id="Img3"/></li>
+              <li><img src="/PROJECT_TO_SIRAJ//main/booksimg/b3.png" alt="Img 4" id="Img4"/></li>
+              <li><img src="/PROJECT_TO_SIRAJ//main/booksimg/b4.png" alt="Img 5" id="Img5"/></li>
+              <li><img src="/PROJECT_TO_SIRAJ//main/booksimg/b5.png" alt="Img 6" id="Img6"/></li>
+              <li><img src="/PROJECT_TO_SIRAJ//main/booksimg/b6.png" alt="Img 7" id="Img7"/></li>
+              <li><img src="/PROJECT_TO_SIRAJ//main/booksimg/b3.png" alt="Img 7" id="Img8"/></li>
+          </ul>
+      </div>
+      
+      <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+      <script type="text/javascript" src="Home.js" ></script>
+      <script>
+          $('.slide').hiSlide();
+      </script>
+       <footer>
+        <p class="label1">&copy; 2023 Mark Francis Sauquillo. All right reserved.</p>
+    </footer>
+</body>
+</html>
